4cf5e22a975a31ada2caf2059af05dc29c135a0d
patiwet <patiwet.boonpeng@gmail.com>
2017-05-05 18:24:05 +0700

    add production build

---
+++ webpack.prod.config.js
@@ -0,0 +1,43 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    main: './src/index.js'
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    },
+    {
+      test: /\.scss$/,
+      loader: 'style-loader!css-loader!sass-loader'
+    }]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    // new webpack.ProvidePlugin({
+    //   'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+    // }),
+    // new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': '"production"'
+      }
+    })
+  ]
+};
